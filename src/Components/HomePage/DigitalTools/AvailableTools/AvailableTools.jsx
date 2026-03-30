5a3820a59a64bf2154629dcd98ce4375c67f7543
piasmajumdar <piasmajumdar@gmail.com>
2026-03-30 19:45:40 +0530

    product card design and show all product

--- src/Components/HomePage/DigitalTools/AvailableTools/AvailableTools.jsx
+++ src/Components/HomePage/DigitalTools/AvailableTools/AvailableTools.jsx
@@ -6,8 +6,8 @@ const AvailableTools = ({ products }) => {
     // console.log(products)
 
     return (
-        <div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
+        <div className='mt-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {products.map(product => <ProductCard key={product.id} product={product}></ProductCard>)}
             </div>
         </div>
--- src/Components/ui/ProductCard.jsx
+++ src/Components/ui/ProductCard.jsx
@@ -1,46 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoCheckmark } from 'react-icons/io5';
 
-const productCard = ({product}) => {
+const productCard = ({ product }) => {
+
+    const [purchased, setPurchased] = useState(false);
+
     // console.log(product)
-
+    // {
+    //     "id": 1,
+    //     "name": "AI Writing Pro",
+    //     "description": "Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.",
+    //     "price": 29,
+    //     "period": "monthly",
+    //     "tag": "Best Seller",
+    //     "tagType": "best-seller",
+    //     "features": [
+    //       "Unlimited AI generations",
+    //       "50+ writing templates",
+    //       "Grammar checker"
+    //     ],
+    //     "icon": "https://i.ibb.co/rGskzQZK/writing-2327400-1.png"
+    //   },
 
 
     return (
-        <div className="card w-96 bg-base-100 shadow-sm mx-auto">
+        <div className="card w-96 bg-base-100 shadow-sm mx-auto hover:shadow-purple-800 hover:scale-102">
             <div className="card-body">
-                <span className="badge badge-xs badge-warning">Most Popular</span>
-                <div className="flex justify-between">
-                    <h2 className="text-3xl font-bold">Premium</h2>
-                    <span className="text-xl">$29/mo</span>
+                <div className='flex justify-end'>
+                    <span className={`badge badge-soft badge-md rounded-full text-sm py-4 px-5 ${product.tagType === "best-seller" ? "badge-warning" : product.tagType === "new" ? "badge-success" : "badge-primary"}`}>{product.tag}</span>
                 </div>
-                <ul className="mt-6 flex flex-col gap-2 text-xs">
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>High-resolution image generation</span>
-                    </li>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>Customizable style templates</span>
-                    </li>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>Batch processing capabilities</span>
-                    </li>
-                    <li>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span>AI-driven image enhancements</span>
-                    </li>
-                    <li className="opacity-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span className="line-through">Seamless cloud integration</span>
-                    </li>
-                    <li className="opacity-50">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                        <span className="line-through">Real-time collaboration tools</span>
-                    </li>
+                <div className='h-15 w-15 border border-gray-300 rounded-full flex justify-center items-center'>
+                    <img className='h-8 w-8' src={product.icon} alt="" />
+                </div>
+                <div className="space-y-2">
+                    <h2 className="text-2xl text-[#101727] font-bold">{product.name}</h2>
+                    <p className="text-[#627382]">{product.description}</p>
+                    <h3 className="text-xl"><span className='font-bold text-2xl text-[#101727]'>${product.price}</span><span className='text-[#627382]'>/{product.period}</span></h3>
+                </div>
+                <ul className="mt-6 flex flex-col gap-2">
+                    {product.features.map((feature, ind) => {
+                        return (
+                            <li key={ind}>
+                                <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                <span className='text-[#627382]'>{feature}</span>
+                            </li>
+                        )
+                    })}
                 </ul>
                 <div className="mt-6">
-                    <button className="btn btn-primary btn-block">Subscribe</button>
+                    {purchased === false ?
+                        <button
+                            onClick={() => setPurchased(true)}
+                            className='btn w-full rounded-full text-white font-medium bg-linear-to-r from-[#4F39F6] to-[#9514FA] hover:scale-105'>
+                            Buy Now
+                        </button>
+                        :
+                        <button
+                            className='btn bg-green-600 w-full rounded-full  text-white font-medium hover:scale-105'>
+                            <IoCheckmark className='scale-150'/>Added to Cart!
+                        </button>
+                    }
                 </div>
             </div>
         </div>
