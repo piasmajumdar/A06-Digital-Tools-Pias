c934642c0845d35f1dcb477abc99f33b7a7be200
piasmajumdar <piasmajumdar@gmail.com>
2026-03-30 20:54:54 +0530

    cart empty

--- src/Components/HomePage/DigitalTools/Cart/Cart.jsx
+++ src/Components/HomePage/DigitalTools/Cart/Cart.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-const Cart = ({selectedTools, setSelectedTools}) => {
+const Cart = ({ selectedTools, setSelectedTools }) => {
     return (
-        <div>
-            <h2>{selectedTools.map(tool=> tool.name)}</h2>
+        <div className='border-2 border-gray-400 rounded-xl p-10'>
+            <h2 className='font-bold text-2xl text-[#101727]'>Your Cart</h2>
+
+            <div>
+                {selectedTools.length == 0 ?
+                    <div>
+                        <MdShoppingCart className='text-[200px]'></MdShoppingCart>
+                        <h2>Your Cart is Empty</h2>
+                    </div>
+                    : 
+                    <div>
+                        <h2>{selectedTools.length}</h2>
+                    </div>
+                }
+            </div>
         </div>
+
     );
 };
 
