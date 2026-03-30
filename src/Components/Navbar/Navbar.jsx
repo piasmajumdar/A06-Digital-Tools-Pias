435c7da28602df47bffc289ea4dbaa0be3c95b05
piasmajumdar <piasmajumdar@gmail.com>
2026-03-30 20:41:33 +0530

    price update in Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -55,8 +55,8 @@ const Navbar = ({ selectedTools }) => {
                             tabIndex={0}
                             className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
                             <div className="card-body">
-                                <span className="text-lg font-bold">8 Items</span>
-                                <span className="text-info">Subtotal: $999</span>
+                                <span className="text-lg font-bold">{selectedTools.length} Items</span>
+                                <span className="text-info">Subtotal: ${selectedTools.reduce((acc, item) => acc+item.price, 0)}</span>
                                 <div className="card-actions">
                                     <button className="btn btn-primary btn-block">View cart</button>
                                 </div>
