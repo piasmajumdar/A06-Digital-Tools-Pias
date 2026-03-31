f9bd5cab0c8d4ec63d3f6b708ecdb7eb3e38696f
piasmajumdar <piasmajumdar@gmail.com>
2026-03-31 18:45:25 +0530

    small fixes in banner for responsiveness

--- src/Components/HomePage/Stats/Stats.jsx
+++ src/Components/HomePage/Stats/Stats.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const Stats = () => {
     return (
         <div className='bg-linear-to-r from-[#4F39F6] to-[#9514FA]'>
-            <div className='max-w-3/4 mx-auto py-15 flex flex-wrap gap-2 justify-around text-center'>
+            <div className='max-w-3/4 mx-auto py-15 flex flex-wrap gap-4 justify-around text-center'>
                 <div className='space-y-1 flex-1'>
                     <h1 className='text-white font-bold text-3xl md:text-6xl'>50K+</h1>
                     <h3 className='text-white/80 font-medium text-2xl'>Active Users</h3>
                 </div>
-                <div className='space-y-1 flex-1 border-x-2 border-x-gray-500'>
+                <div className='space-y-1 flex-1 sm:border-x-2 sm:border-x-gray-500'>
                     <h1 className='text-white font-bold text-3xl md:text-6xl'>200+</h1>
                     <h3 className='text-white/80 font-medium text-2xl'>Premium Tools</h3>
                 </div>
